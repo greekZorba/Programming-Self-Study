e56bebe7b112ae38a3117834c38d176f569a52cb
jinhak Kim <smiling.jinhak@gmail.com>
2018-09-18 20:54:09 +0900

    관계연산자 Xor 정리 중
    배타적 논리합이란?
    1 xor 1 = 0
    0 xor 0 = 0
    1 xor 0 = 1
    0 xor 1 = 1

---
+++ src/com/concept/XorStudy.java
@@ -0,0 +1,14 @@
+package com.concept;
+
+
+public class XorStudy {
+
+    public static void main(String[] args){
+
+        int a = 1;
+        int b = 1;
+        System.out.println(a^b);
+
+    }
+
+}
