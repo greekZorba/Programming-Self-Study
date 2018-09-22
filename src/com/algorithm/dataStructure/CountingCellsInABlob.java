21e694ba9a6c5252afa07569c91ade48e3b6e676
jinhak Kim <smiling.jinhak@gmail.com>
2018-09-22 11:30:16 +0900

    Blob을 세주는 알고리즘

---
+++ src/com/algorithm/dataStructure/CountingCellsInABlob.java
@@ -0,0 +1,45 @@
+package com.algorithm.dataStructure;
+
+/********************************************************************
+ *
+ *
+ * Blob은 서로 연결된 이미지 pixel들의 집합을 말한다.
+ *
+ *
+ *********************************************************************/
+public class CountingCellsInABlob {
+    private static int BACKGROUND_COLOR = 0;
+    private static int IMAGE_COLOR = 1;
+    private static int ALREADY_COUNTED = 2;
+
+    static int[][] map;
+    public static void main(String[] args){
+
+        map = new int[][]{
+                {1,0,0,0,0,0,0,1},
+                {0,1,1,0,0,1,0,0},
+                {1,1,0,0,1,0,1,0},
+                {0,0,0,0,0,1,0,0},
+                {0,1,0,1,0,1,0,0},
+                {0,1,0,1,0,1,0,0},
+                {1,0,0,0,1,0,0,1},
+                {0,1,1,0,0,1,1,1}
+        };
+
+        System.out.println("Blob의 셀 수: "+countCells(3, 5));
+    }
+
+    static int countCells(int x, int y){
+        if(x < 0 || x > map.length-1 || y < 0 || y > map[x].length-1 ){
+            return 0;
+        }else if(map[x][y] != 1 ){ // 벽이거나 이미 카운팅한 셀
+            return 0;
+        }else{
+            map[x][y] = ALREADY_COUNTED;
+            return 1 + countCells(x-1, y) + countCells(x-1, y+1) + countCells(x, y+1)
+                    + countCells(x+1, y+1) + countCells(x+1, y) + countCells(x+1, y-1)
+                    + countCells(x, y-1) + countCells(x-1, y-1);
+        }
+
+    }
+}
