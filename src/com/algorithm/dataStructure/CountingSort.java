355204fb84ffa3b886ad9b58a33ccb3a3c282ccc
jinhak Kim <smiling.jinhak@gmail.com>
2018-10-15 21:55:52 +0900

    CountingSort 주석 설명 commit

--- src/com/algorithm/dataStructure/CountingSort.java
+++ src/com/algorithm/dataStructure/CountingSort.java
@@ -1,5 +1,22 @@
 package com.algorithm.dataStructure;
 
+/********************************************************************
+ *
+ *
+ * CountingSort
+ *
+ * CountingSort는 RadixSort와 같이 다른 정렬법과 다르게 non-comparison-sort이다
+ * 입력값에 정해진 범위가 있다는 전제 하에 정렬한다
+ * 입력값을 index로 만들어서 같은 입력값이 나올 때마다 입력값을 index로 하는 배열을 +1씩 한다
+ * 구해진 counting 배열을 누적으로 더해주면 가장 앞에 나오는 숫자부터 누적으로 더해지는데
+ * 이는 정렬을 하는데 어느자리로 들어갈 지 알려준다
+ *
+ * CountingSort는 StableSort이기 때문에 입력순서와 출력순서는 같아야한다.
+ * 초기 입력 배열을 거꾸러 읽어주면서 정렬을 하면 동일한 수라도 나중에 입력된 배열값이 나중에 출력된다
+ *
+ * 출처: inflearn 권오흠 교수님 알고리즘 강좌
+ *
+ *********************************************************************/
 public class CountingSort {
 
     private static int[] inputArray;
