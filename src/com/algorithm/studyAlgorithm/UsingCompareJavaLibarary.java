83f9d7dcebd0f196a09cc4f9d6403928b0063135
jinhak Kim <smiling.jinhak@gmail.com>
2018-10-16 23:04:28 +0900

    compare 라이브러리 클래스 생성

---
+++ src/com/algorithm/studyAlgorithm/UsingCompareJavaLibarary.java
@@ -0,0 +1,13 @@
+package com.algorithm.studyAlgorithm;
+
+public class UsingCompareJavaLibarary implements Comparable<Integer>{
+
+    public static void main(String[] args){
+
+    }
+
+    @Override
+    public int compareTo(Integer o) {
+        return 0;
+    }
+}
