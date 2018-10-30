2b69fe5bcb21f836a7fed47446703a6cbc0e57a1
jinhak Kim <smiling.jinhak@gmail.com>
2018-10-30 23:39:05 +0900

    Codility 알고리즘 풀이
    레슨마다 있는 문제

---
+++ src/com/algorithm/studyAlgorithm/codility/OddOccurrencesInArray.java
@@ -0,0 +1,42 @@
+package com.algorithm.studyAlgorithm.codility;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+/**
+ * Arrays 문제
+ *
+ * */
+public class OddOccurrencesInArray {
+
+    public static void main(String[] args){
+        int[] A = new int[]{9,3,9,3,9,7,9};
+        System.out.println(solution(A));
+    }
+
+    private static int solution(int[] A){
+        Map<Integer, Integer> mapArray = new HashMap<>();
+
+        for(int i=0; i<A.length; i++){
+
+            if(mapArray.containsKey(A[i])){
+                mapArray.put(A[i], mapArray.get(A[i])+1);
+            }else{
+                mapArray.put(A[i], 1);
+            }
+
+        }
+
+        int result = 0;
+        for(int i=0; i<A.length; i++){
+
+            if(mapArray.get(A[i])%2 != 0){
+                result = A[i];
+                break;
+            }
+        }
+
+        return result;
+    }
+}
---
+++ src/com/algorithm/studyAlgorithm/codility/PermMissingElem.java
@@ -0,0 +1,29 @@
+package com.algorithm.studyAlgorithm.codility;
+
+/**
+ * TimeComplexity 문제
+ *
+ * */
+public class PermMissingElem {
+
+    public static void main(String[] args){
+        int[] A = new int[]{2,3,1,5};
+        System.out.println(solution(A));
+    }
+
+    private static int solution(int[] A){
+        int[] countArray = new int[A.length+2];
+        for(int i=0; i<A.length; i++){
+            countArray[A[i]]++;
+        }
+
+        int result = 0;
+        for(int i=1; i<countArray.length; i++){
+            if(countArray[i] == 0){
+                result = i;
+            }
+        }
+
+        return result;
+    }
+}
