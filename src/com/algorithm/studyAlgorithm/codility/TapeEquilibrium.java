8a087d92901890022b87f371c58c5b08052edd11
greekZorba <smiling.jinhak@gmail.com>
2018-10-31 17:10:37 +0900

    시간복잡도 문제

---
+++ src/com/algorithm/studyAlgorithm/codility/TapeEquilibrium.java
@@ -0,0 +1,37 @@
+package src.com.algorithm.studyAlgorithm.codility;
+
+/**
+ * TimeComplexity 문제
+ *
+ * */
+public class TapeEquilibrium {
+
+    public static void main(String[] args){
+//        int[] A = new int[]{-10, -20, -30, -40, 100};
+        int[] A = new int[]{-1000,1000,1000};
+        System.out.println(solution(A));
+    }
+
+    private static int solution(int[] A){
+        int originalSum = 0;
+        for(int i=0; i<A.length; i++){
+            originalSum += A[i];
+        }
+
+        int max = Integer.MAX_VALUE;
+        int afterSum = 0;
+        for(int i=0; i<A.length-1; i++){
+            originalSum -= A[i];
+            afterSum += A[i];
+
+            if(max > Math.abs(afterSum-originalSum)){
+                max = Math.abs(afterSum-originalSum);
+            }
+        }
+
+        if(A.length == 2){
+            max = Math.abs(A[0]-A[1]);
+        }
+        return max;
+    }
+}
