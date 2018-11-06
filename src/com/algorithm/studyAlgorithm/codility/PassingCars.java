9c8cf6ff0fede7e549f63f80fbcb8d17856a5d38
jinhak Kim <smiling.jinhak@gmail.com>
2018-11-06 23:32:11 +0900

    codility prefix sums 문제 풀이
    문제는 PassingCars

---
+++ src/com/algorithm/studyAlgorithm/codility/PassingCars.java
@@ -0,0 +1,33 @@
+package com.algorithm.studyAlgorithm.codility;
+
+/**
+ * Prefix Sums 문제
+ *
+ * */
+public class PassingCars {
+
+    public static void main(String[] args){
+        int[] A = new int[]{0,1,0,1,1};
+        System.out.println(solution(A));
+    }
+
+    private static int solution(int[] A){
+        int travleEastCar = 0;
+        int pairCarSum = 0;
+
+        for(int i=0; i<A.length; i++){
+
+            if(A[i] == 0){
+                travleEastCar++;
+            }else{
+                pairCarSum += travleEastCar;
+                if(pairCarSum > 1000000000){
+                    pairCarSum = -1;
+                    break;
+                }
+            }
+        }
+
+        return pairCarSum;
+    }
+}
