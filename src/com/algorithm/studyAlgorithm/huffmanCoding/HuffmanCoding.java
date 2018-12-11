c19c65ac3d18ce5f5758887c890d17f007677e53
jinhak Kim <smiling.jinhak@gmail.com>
2018-12-11 23:08:17 +0900

    허프만코딩 구현 v.2

--- src/com/algorithm/studyAlgorithm/huffmanCoding/HuffmanCoding.java
+++ src/com/algorithm/studyAlgorithm/huffmanCoding/HuffmanCoding.java
@@ -9,11 +9,41 @@ public class HuffmanCoding {
 
     private List<Run> runs = new ArrayList<>();
 
+    private Heap<Run> heap; /** minimum heap */
+    private Run theRoot = null; /** root of Huffman tree */
+
     private void collectionRuns(RandomAccessFile fIn) throws IOException{
         /**
          * 데이터 파일 fIn에 등장하는 모든 run들과 각각의 등장횟수를 count를 하여
          * ArrayList runs에 저장한다.
          * */
+
+    }
+
+    private void createHuffmanTree(){
+        heap = new Heap<Run>();
+        /**
+         * 1. store all runs into heap
+         * 2. while the heap size > 1 do
+         *    (1) perform extract min two times
+         *    (2) make a combine tree
+         *    (3) insert the combined tree into the heap
+         * 3. Let the root be the root of the tree
+         * */
+    }
+
+    private void assignCodeword(Run run, String codeword, String codewordLen){
+        if(run.left == null && run.right == null){
+            run.codeword = codeword;
+            run.codewordLen = codewordLen;
+        }
+    }
+
+    public void compressFile(RandomAccessFile fIn) throws IOException{
+
+        collectionRuns(fIn);
+        createHuffmanTree();
+        assignCodeword(theRoot, "0", "0");
     }
 
     public static void main(String[] args){
@@ -21,7 +51,7 @@ public class HuffmanCoding {
         RandomAccessFile fIn;
         try{
             fIn = new RandomAccessFile("sample.txt", "r");
-            app.collectionRuns(fIn);
+            app.compressFile(fIn);
             fIn.close();
         }catch (IOException io){
             System.out.println("Cannot open sample.txt");
--- src/com/algorithm/studyAlgorithm/huffmanCoding/Run.java
+++ src/com/algorithm/studyAlgorithm/huffmanCoding/Run.java
@@ -1,10 +1,22 @@
 package com.algorithm.studyAlgorithm.huffmanCoding;
 
-public class Run {
+public class Run implements Comparable<Run>{
 
+    public Run left;
+    public Run right;
     public byte symbol;
-    public int runLen;
-    public int freq;
+    public int runLen; // 문장 길이
+    public int freq; // 빈도
 
-    /** 적절한 생성자와 equals 메서드를 완성하라 */
+    /** 트리의 노드로 사용하기 위해서 왼쪽 자식과 오른쪽 자식 노드 필드를 추가한다. */
+
+    /** 노드에 부여된 codeword를 저장하기 위한 필드들을 다음과 같이 추가한다. */
+    public String codeword; // 부여된 codeword를 32비트 정수로 저장
+    public String codewordLen; // 부여된 codeword의 길이. 즉, codewordLen비트가 실제 codeword
+
+    /** 비교의 기준은 freq이다. */
+    @Override
+    public int compareTo(Run o) {
+        return 0;
+    }
 }
