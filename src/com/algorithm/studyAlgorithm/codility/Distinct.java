a41d707e8305b3aca8c402cd2ef05e21fb49e046
greekZorba <smiling.jinhak@gmail.com>
2019-01-08 17:25:27 +0900

    codility problem
    Subject: sorting
    - Distinct

---
+++ src/com/algorithm/studyAlgorithm/codility/Distinct.java
@@ -0,0 +1,22 @@
+package com.algorithm.studyAlgorithm.codility;
+
+import java.util.LinkedHashSet;
+
+public class Distinct {
+
+    public int solution(int[] A){
+        LinkedHashSet<Integer> distinctArray = new LinkedHashSet<>();
+        for(int i =0; i<A.length; i++){
+            distinctArray.add(A[i]);
+        }
+
+        return distinctArray.size();
+    }
+
+
+    public static void main(String[] args){
+        int[] A = new int[]{2,1,1,2,3,1};
+        Distinct distinct = new Distinct();
+        System.out.println(distinct.solution(A));
+    }
+}
