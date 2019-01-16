b520fde9c10d8858893deab5237f6d022d707dcb
jinhak Kim <smiling.jinhak@gmail.com>
2019-01-16 22:57:02 +0900

    GenomicRangeQuery 시간복잡도까지 맞춘 풀이법

--- src/com/algorithm/studyAlgorithm/codility/GenomicRangeQuery.java
+++ src/com/algorithm/studyAlgorithm/codility/GenomicRangeQuery.java
@@ -45,6 +45,51 @@ public class GenomicRangeQuery {
 
     }
 
+    /** 시간 복잡도 O(N+M)*/
+    public int[] solution2(String S, int[] P, int[] Q) {
+        //used jagged array to hold the prefix sums of each A, C and G genoms
+        //we don't need to get prefix sums of T, you will see why.
+        int[][] genoms = new int[3][S.length()+1];
+        //if the char is found in the index i, then we set it to be 1 else they are 0
+        // 3 short values are needed for this reason
+        short a, c, g;
+        for (int i=0; i<S.length(); i++) {
+            a = 0; c = 0; g = 0;
+            if ('A' == (S.charAt(i))) {
+                a=1;
+            }
+            if ('C' == (S.charAt(i))) {
+                c=1;
+            }
+            if ('G' == (S.charAt(i))) {
+                g=1;
+            }
+            //here we calculate prefix sums. To learn what's prefix sums look at here https://codility.com/media/train/3-PrefixSums.pdf
+            genoms[0][i+1] = genoms[0][i] + a;
+            genoms[1][i+1] = genoms[1][i] + c;
+            genoms[2][i+1] = genoms[2][i] + g;
+        }
+
+        int[] result = new int[P.length];
+        //here we go through the provided P[] and Q[] arrays as intervals
+        for (int i=0; i<P.length; i++) {
+            int fromIndex = P[i]+1;
+            int toIndex = Q[i]+1;
+            //if the substring contains a, then genoms[0][toIndex] - genoms[0][fromIndex-1] > 0
+            if (genoms[0][toIndex] - genoms[0][fromIndex-1] > 0) {
+                result[i] = 1;
+            } else if (genoms[1][toIndex] - genoms[1][fromIndex-1] > 0) {
+                result[i] = 2;
+            } else if (genoms[2][toIndex] - genoms[2][fromIndex-1] > 0) {
+                result[i] = 3;
+            } else {
+                result[i] = 4;
+            }
+        }
+
+        return result;
+    }
+
 
 
     public static void main(String[] args) {
@@ -58,6 +103,11 @@ public class GenomicRangeQuery {
         for(int out: output){
             System.out.print(out + " ");
         }
+        System.out.println();
 
+        int[] output2 = genomicRangeQuery.solution2(S,P,Q);
+        for(int out: output2){
+            System.out.print(out + " ");
+        }
     }
 }
