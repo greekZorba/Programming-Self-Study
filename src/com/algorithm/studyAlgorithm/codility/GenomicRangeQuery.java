b56a0c934825474766cb63cda05d407efb907ffd
greekZorba <smiling.jinhak@gmail.com>
2019-01-17 09:06:44 +0900

    Update GenomicRangeQuery.java
    add reamark

--- src/com/algorithm/studyAlgorithm/codility/GenomicRangeQuery.java
+++ src/com/algorithm/studyAlgorithm/codility/GenomicRangeQuery.java
@@ -45,13 +45,25 @@ public class GenomicRangeQuery {
 
     }
 
-    /** 시간 복잡도 O(N+M)*/
+    /** 시간 복잡도 O(N+M)
+     *
+     * Reference: https://github.com/ZRonchy/Codility/blob/master/Lesson3/GenomicRangeQuery.java
+     * */
     public int[] solution2(String S, int[] P, int[] Q) {
         //used jagged array to hold the prefix sums of each A, C and G genoms
         //we don't need to get prefix sums of T, you will see why.
+        /**
+         * 여기서 genoms는 각각의 A, C, G의 prefix sum들을 가진 배열이다.
+         * T의 prefix sum들은 필요없다. (코드를 보면 알 수 있음)
+         * */
         int[][] genoms = new int[3][S.length()+1];
-        //if the char is found in the index i, then we set it to be 1 else they are 0
-        // 3 short values are needed for this reason
+
+        /**
+         * 인덱스 i에서 문자를 발견하면 해당 배열에 1을 대입 아니면 0
+         * A를 발견하면 genoms[0] 행에 들어감
+         * B를 발견하면 genoms[1] 행에 들어감
+         * C를 발견하면 genoms[2] 행에 들어감
+         * */
         short a, c, g;
         for (int i=0; i<S.length(); i++) {
             a = 0; c = 0; g = 0;
@@ -64,18 +76,24 @@ public class GenomicRangeQuery {
             if ('G' == (S.charAt(i))) {
                 g=1;
             }
-            //here we calculate prefix sums. To learn what's prefix sums look at here https://codility.com/media/train/3-PrefixSums.pdf
+
             genoms[0][i+1] = genoms[0][i] + a;
             genoms[1][i+1] = genoms[1][i] + c;
             genoms[2][i+1] = genoms[2][i] + g;
         }
 
         int[] result = new int[P.length];
-        //here we go through the provided P[] and Q[] arrays as intervals
+        /**
+         * P[]의 값과 Q[]의 값을 시작과 끝의 인덱스로 계산한다.
+         * */
         for (int i=0; i<P.length; i++) {
             int fromIndex = P[i]+1;
             int toIndex = Q[i]+1;
-            //if the substring contains a, then genoms[0][toIndex] - genoms[0][fromIndex-1] > 0
+            /**
+             * 만약 끝과 시작의 인덱스 사이에 문자가 발견된 적 있다면
+             * genoms[?][toIndex] - genoms[?][fromIndex-1] > 0 이 되어야 한다.
+             * fromIndex-1부터 시작해야 시작 인덱스의 값(fromIndex)과 마지막 인덱스의 값 사이를 비교할 수 있다.
+             * */
             if (genoms[0][toIndex] - genoms[0][fromIndex-1] > 0) {
                 result[i] = 1;
             } else if (genoms[1][toIndex] - genoms[1][fromIndex-1] > 0) {
